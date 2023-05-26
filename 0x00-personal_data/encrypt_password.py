dd1966ab974c35b34c3171463e11df1044ffdd31
queensk <queenskisivuli@gmail.com>
2023-05-26 05:06:20 +0300

    hash passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,11 @@ def hash_password(password: str) -> bytes:
     password_bytes = password.encode()
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password_bytes, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    returns a boolean indicating if the password matches the hashed password
+    """
+    password_bytes = password.encode()
+    return bcrypt.checkpw(password_bytes, hashed_password)
