168b52ae4b22bc7a109f2e82afab7590fe0976d9
queensk <queenskisivuli@gmail.com>
2023-05-26 03:17:58 +0300

    fix typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,14 @@
 Regex-ing
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     returns the log message obfuscated:
     """
