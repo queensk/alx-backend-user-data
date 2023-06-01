71e37d9df476bd759545075d0c6a379e4c3b6a0e
queensk <queenskisivuli@gmail.com>
2023-06-01 21:39:17 +0300

    Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,22 @@
 class BasicAuth module
 """
 from .auth import Auth
+from re import fullmatch
 
 
 class BasicAuth(Auth):
     """
     Create a class BasicAuth that inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header
+        """
+        if isinstance(authorization_header, str):
+            pattern = r'Basic (?P<token>.+)'
+            field_match = fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group("token")
+        return None
