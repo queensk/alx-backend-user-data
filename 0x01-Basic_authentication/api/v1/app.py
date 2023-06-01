830b91a8a2d744f2e9996328062a1c3826dbccf7
queensk <queenskisivuli@gmail.com>
2023-06-01 21:02:14 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ AUTH_TYPE = os.getenv('AUTH_TYPE', 'auth')
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+class BasicAuth module
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Create a class BasicAuth that inherits from Auth
+    """
+    pass
