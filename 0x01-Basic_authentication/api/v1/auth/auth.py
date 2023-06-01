2b4a68a76ad3adcb67069075ef11bb7710e50d45
queensk <queenskisivuli@gmail.com>
2023-06-01 17:14:44 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,8 +31,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns None - request will be the Flask request object.
+        Request validation.
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
