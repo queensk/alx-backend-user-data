f662536b779b951eddc51aae85e1692f82f56a76
queensk <queenskisivuli@gmail.com>
2023-06-01 12:33:42 +0300

    handle unauthorized routes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    unauthorized
+    Return:
+      - Unauthorized error.
+    """
+    abort(401)
