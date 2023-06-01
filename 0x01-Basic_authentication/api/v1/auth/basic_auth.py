ab2042395cfc67c7ea4195412ab747622114401e
queensk <queenskisivuli@gmail.com>
2023-06-01 22:03:53 +0300

    base 64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ class BasicAuth module
 """
 from .auth import Auth
 from re import fullmatch
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,4 +23,19 @@ class BasicAuth(Auth):
             field_match = fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group("token")
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        decoded value of a Base64 string
+        """
+        if base64_authorization_header is not None and isinstance(
+                base64_authorization_header, str):
+            try:
+                decode_str = base64.b64decode(
+                    base64_authorization_header, validate=True)
+                return decode_str.decode("utf-8")
+            except (binascii.Error, UnicodeDecodeError):
+git ad                return None
         return None
