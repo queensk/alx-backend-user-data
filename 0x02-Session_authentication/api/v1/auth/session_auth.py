6933985013869bc4877295636371876b75497943
queensk <queenskisivuli@gmail.com>
2023-06-03 05:45:55 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,11 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        returns a User ID based on a Session ID
+        """
+        if isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
+        return None
