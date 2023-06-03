893e14410df72b46277aadc948f4e051c84c2f61
queensk <queenskisivuli@gmail.com>
2023-06-03 04:54:55 +0300

    Et moi et moi et moi

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,13 +51,13 @@ def authenticate_user():
             '/api/v1/forbidden/',
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
-
+            request.current_user = user
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
