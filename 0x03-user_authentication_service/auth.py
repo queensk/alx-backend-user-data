d64f0a17ad4caab108977d48bbaf78d05b795366
queensk <queenskisivuli@gmail.com>
2023-06-10 02:02:57 +0300

    password update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -148,16 +148,16 @@ class Auth:
         Raises:
             ValueError: If the reset_token is invalid.
         """
+        user = None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             user = None
-        if not user:
+        if user is None:
             raise ValueError()
-
-        hashed_password = _hash_password(password)
+        new_password_hash = _hash_password(password)
         self._db.update_user(
             user.id,
-            hashed_password=hashed_password,
+            hashed_password=new_password_hash,
             reset_token=None,
         )
