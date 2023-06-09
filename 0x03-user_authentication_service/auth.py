f8b181fbb68b616d4df7adebd567561abe07e10f
queensk <queenskisivuli@gmail.com>
2023-06-10 00:26:03 +0300

    check valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,3 +49,16 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Checks if the login credentials are valid.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password)
+        except NoResultFound:
+            return False
