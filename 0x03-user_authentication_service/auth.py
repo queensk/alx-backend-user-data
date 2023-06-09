866f915fd0084e409079fac438fe6eb91b8cc560
queensk <queenskisivuli@gmail.com>
2023-06-10 02:01:03 +0300

    password update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -136,3 +136,28 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update the password of the user associated with the given reset_token.
+
+        Args:
+            reset_token: The reset token of the user.
+            password: The new password to update.
+
+        Raises:
+            ValueError: If the reset_token is invalid.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if not user:
+            raise ValueError()
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=hashed_password,
+            reset_token=None,
+        )
