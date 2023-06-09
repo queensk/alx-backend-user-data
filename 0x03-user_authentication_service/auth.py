633e629c0c9b11efbb0b2eab62f33524475f6f3c
queensk <queenskisivuli@gmail.com>
2023-06-10 01:49:45 +0300

    get reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -130,11 +130,9 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return None
-
-        if not user:
+            user = None
+        if user is None:
             raise ValueError()
-
-        reset_token = str(uuid4())
+        reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
