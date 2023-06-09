2011b7732b799e0b1aa9bf1f7549cec4cf735b25
queensk <queenskisivuli@gmail.com>
2023-06-10 01:54:43 +0300

    reset user password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,22 @@ def profile() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token() -> str:
+    """
+    Generate a reset password token for the given email.
+
+    Returns:
+        A JSON payload containing the email and reset_token.
+    """
+    email = request.form.get("email")
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
