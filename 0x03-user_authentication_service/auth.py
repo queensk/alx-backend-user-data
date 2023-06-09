890ee0ea4fb59222021be01c209855fc24f2a31b
queensk <queenskisivuli@gmail.com>
2023-06-10 01:03:07 +0300

    create app session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,16 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
             return session_id
         return None
+
+    def get_user_from_session_id(self, session_id):
+        """
+        Get a user session
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
