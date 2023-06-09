a13dcd2b6c8fb607378d6a89b8aecc6faec8d014
queensk <queenskisivuli@gmail.com>
2023-06-10 00:48:55 +0300

    create a session id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Flask App
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -40,6 +40,18 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+        return response
+
+    abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
