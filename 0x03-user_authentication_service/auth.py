279b5a159d11b3acf7069572a1ee4b955ec37a2c
queensk <queenskisivuli@gmail.com>
2023-06-10 00:32:58 +0300

    create a session for user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,16 @@ class Auth:
                     user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a session for the user with the given email.
+        """
+        user = self._db.find_user_by(email=email)
+
+        if user:
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+
+        return None
